f914ad3dd780909a7a31845b8f1e9f7acd05ac3c
mongomoves <mongomoves@gmail.com>
2018-08-08 12:20:22 +0200

    fixed warnings, still wrong

--- Challenges/LcdDisplay.cpp
+++ Challenges/LcdDisplay.cpp
@@ -5,18 +5,19 @@
 #include <stdio.h>
 #include <vector>
 #include <string>
+#include <limits>
 
 #define NUMBER_VEC std::vector<std::vector<char>>
 enum ROW_TYPE {CENTER_BAR, LEFT_BAR, RIGHT_BAR, BOTH_BAR, BLANK};
 
 //Prints the rows in the vectors, and line breaks everytime it's done the same as the number of digits.
-void printNumber(std::vector<NUMBER_VEC> &numbers, unsigned rowLength, unsigned size)
+void printNumber(std::vector<NUMBER_VEC> &numbers, int rowLength, int size)
 {
 	for (int i = 0; i < 2 * size + 3; i++)
 	{
-		for (int j = 0; j < numbers.size(); j++)
+		for (std::size_t j = 0; j < numbers.size(); j++)
 		{
-			for (int k = 0; k < numbers.at(j).at(i).size(); k++)
+			for (std::size_t k = 0; k < numbers.at(j).at(i).size(); k++)
 			{
 				printf("%c", numbers.at(j).at(i).at(k));
 			}
@@ -26,7 +27,7 @@ void printNumber(std::vector<NUMBER_VEC> &numbers, unsigned rowLength, unsigned 
 }
 
 //Fills a row with filler stuff.
-void fill_row(std::vector<char> &row, unsigned size, char filler)
+void fill_row(std::vector<char> &row, int size, char filler)
 {
 	for (int i = 0; i < size; i++)
 	{
@@ -35,7 +36,7 @@ void fill_row(std::vector<char> &row, unsigned size, char filler)
 }
 
 //Creates a row of the enum type. Size says how much filler is needded for blanks and '-'.
-std::vector<char> create_row(ROW_TYPE row_type, unsigned size)
+std::vector<char> create_row(ROW_TYPE row_type, int size)
 {
 	std::vector<char> row;
 	switch (row_type)
@@ -70,7 +71,7 @@ std::vector<char> create_row(ROW_TYPE row_type, unsigned size)
 std::vector<int> int_to_array(std::string input)
 {
 	std::vector<int> numbers;
-	for (int i = 0; i < input.length(); i++)
+	for (std::size_t i = 0; i < input.length(); i++)
 	{
 		numbers.push_back(input[i] - '0');
 	}
@@ -79,7 +80,7 @@ std::vector<int> int_to_array(std::string input)
 
 int main()
 {
-	const int MAX_NUMBER_LENGTH = 8; //Limit according to assignment is 99 999 999
+	const int MAX_NUMBER_LENGTH = std::numeric_limits<char>::max(); //Limit according to assignment is 99 999 999
 	//The design of the various digits.
 	std::vector<ROW_TYPE> ZERO = { CENTER_BAR, BOTH_BAR, BLANK, BOTH_BAR, CENTER_BAR };
 	std::vector<ROW_TYPE> ONE = { BLANK, RIGHT_BAR, BLANK, RIGHT_BAR, BLANK };
@@ -96,19 +97,19 @@ int main()
 		ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE };
 
 	NUMBER_VEC number;						//2d vector, to hold a digit
-	unsigned s;								//From input, size of output
+	int s;								//From input, size of output
 	char n[MAX_NUMBER_LENGTH];				//From input, the number
 	std::vector<int> numbers;				//Vector for the individual numbers in the input
 	std::vector<char> row;					//One row in a digit, ex. CENTER_BAR
 	std::vector<NUMBER_VEC> allNumbers;		//All the digits to be outputted
 	//Scanning for input
-	while (scanf("%d %7s", &s, n) > 0)
+	while (scanf("%d %s", &s, n) > 0)
 	{
 		if (s == 0) { return 0; }					//If end of input
-		if (s > 10) continue;
-		if (std::stoi(n) > 10000000) continue;		//If the number is bigger than the limit
+		//if (s > 10) continue;
+		//if (std::stoi(n) > 10000000) continue;		//If the number is bigger than the limit
 		numbers = int_to_array(std::string(n));
-		for (int i = 0; i < numbers.size(); i++)	//Create each number
+		for (std::size_t i = 0; i < numbers.size(); i++)	//Create each number
 		{
 			for (int j = 0; j < 5; j++)				//We know the original digit design is only size 5.
 			{
