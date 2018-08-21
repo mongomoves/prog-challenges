ba7421847f32ce9a68e7d52a5a0bae82bd8436bd
mongomoves <mongomoves@gmail.com>
2018-08-21 12:18:17 +0200

    lil comments

--- Challenges/Jollyjumpers.cpp
+++ Challenges/Jollyjumpers.cpp
@@ -1,5 +1,12 @@
 /*
     ID: 10038
+    Determine if sequence of numbers is jolly.
+    A sequence of n > 0 integers is called a jolly jumper if the 
+    absolute values of the differences between successive 
+    elements take on all possible values 1 through n − 1.
+
+    Each line of input contains an integer n < 3,000 followed by 
+    n integers representing the sequence
 */
 
 #include <iostream>
@@ -7,6 +14,7 @@
 #include <string>
 #include <sstream>
 
+//Make a vector with n - 1 booleans
 void fill_bool_vec(std::vector<bool> &vec, int n) 
 {
     vec.clear();
@@ -16,6 +24,7 @@ void fill_bool_vec(std::vector<bool> &vec, int n)
     }
 }
 
+//Line from input to int vector.
 void sstream_to_vec(std::stringstream &ss, std::vector<int> &vec)
 {
     int nbr;
@@ -28,30 +37,32 @@ void sstream_to_vec(std::stringstream &ss, std::vector<int> &vec)
 
 int main()
 {
-    std::vector<int> numbers;
-    std::vector<bool> bool_vec;
+    std::vector<int> numbers;       //The numbers from input, first element is "n".
+    std::vector<bool> bool_vec;     //Vec to check differences against
     std::string line;
     int diff;
     bool jolly;
-    while(std::getline(std::cin, line))
+    while(getline(std::cin, line))  //For each line of input
     {
         std::stringstream strstream(line);
         sstream_to_vec(strstream, numbers);
         jolly = true;
         if(numbers.empty()) continue;
-        if(numbers[0] <= 0 || numbers[0] >= 3000) continue; //N is within bounds
+        if(numbers[0] <= 0 || numbers[0] >= 3000) continue; //Check if N is within bounds
         fill_bool_vec(bool_vec, numbers[0]);
-        for(auto it = numbers.begin() + 1; it != numbers.end(); ++it)
+        for(auto it = numbers.begin() + 1; it != numbers.end(); ++it) //Iterate number sequence (skipping "n")
         {
-            if(std::next(it) != numbers.end()) 
+            if(std::next(it) != numbers.end())  //If not on last element
             {
-                diff = abs(*it - *(it + 1));
-                if(diff >= numbers[0]) jolly = false;
-                if(bool_vec[diff - 1]) bool_vec[diff - 1] = false;
+                diff = abs(*it - *(it + 1));            //Difference between current number and the following
+                if(diff >= numbers[0]) jolly = false;   //If bigger or equal to "n" it can't be jolly!
+                if(bool_vec[diff - 1]) bool_vec[diff - 1] = false; //Flip to false if diff is part of the jolly sequence
             }
         }
         if(jolly)
         {
+            //If any "number" is still true, then the sequence is not jolly.
+            //Because all values in the range must be used.
             for(auto it = bool_vec.begin(); it != bool_vec.end(); ++it)
             {
                 if(*it) jolly = false;
@@ -60,4 +71,5 @@ int main()
         if(jolly) std::cout << "Jolly\n";
         else std::cout << "Not jolly\n";
     }
+    return 0;
 }
